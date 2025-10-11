b20abe185d4448befde2f28d25046b9d5e7c8e74
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-10-11 23:14:02 +0200

    fix: reorder imports and clean up unused code in GalleryView and NotFoundView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import projectsData from '../data/projects.json'
 import type { Project } from '../types/project'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
