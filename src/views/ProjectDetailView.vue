3bfaef44173c4f38057badadc5b75a622b379201
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-10-11 22:58:35 +0200

    feat: restructure project data to include studies with titles and descriptions, and update gallery and detail views accordingly

--- src/views/ProjectDetailView.vue
+++ src/views/ProjectDetailView.vue
@@ -1,6 +1,16 @@
 <template>
   <div v-if="project">
+    <!-- Show StudiesDetail component for projects with studies -->
+    <StudiesDetail
+      v-if="project.studies"
+      :title="project.title"
+      :description="project.description"
+      :studies="project.studies"
+      :video="project.video"
+    />
+    <!-- Show regular ProjectDetail component for projects with images -->
     <ProjectDetail
+      v-else-if="project.images"
       :title="project.title"
       :description="project.description"
       :images="project.images"
@@ -16,6 +26,7 @@
 
 <script setup lang="ts">
 import ProjectDetail from '@/components/ProjectDetail.vue'
+import StudiesDetail from '@/components/StudiesDetail.vue'
 import projectsData from '@/data/projects.json'
 import type { Project } from '@/types/project'
 import { computed, provide } from 'vue'
