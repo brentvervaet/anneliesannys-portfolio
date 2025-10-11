563035df994a34feb3c6bc93e72b05416b58f7be
Brent Vervaet <94802036+brentvervaet@users.noreply.github.com>
2025-10-11 15:24:35 +0200

    Feat/2 about (#16)
    * chore: remove redundant paragraphs from AboutView.vue
    
    * main -> about (#15)
    
    * fix route for vercel (#9)
    
    * fix: update HTML structure and enhance SEO metadata in index.html
    
    * fix: add Vercel configuration for URL rewrites to index.html
    
    * fix: update HTML structure and enhance SEO metadata in index.html (#10)
    
    * gallery added (#11)
    
    * feat: add gallery navigation link to AppHeader component
    
    * feat: add GalleryView component and route for image gallery
    
    * fix: adjust gallery grid gap to 0px for improved layout consistency
    
    * Feat/3 gallery (#12) (#13)
    
    * feat: add gallery navigation link to AppHeader component
    
    * feat: add GalleryView component and route for image gallery
    
    * fix: adjust gallery grid gap to 0px for improved layout consistency
    
    * chore: remove redundant paragraphs from AboutView.vue (#14)
    
    * Refactor code structure for improved readability and maintainability
    
    * feat: update AboutView with personal introduction and project timeline details
    
    * chore: update image asset for improved visual quality

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,29 +1,172 @@
 <template>
   <div class="about">
     <section class="about-content">
-      <div class="about-image">
-        <!-- <img src="/images/About/anneliesannys.webp" alt="Annelies Annys" /> -->
-      </div>
       <div class="about-text">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
+        <div class="profile-image">
+          <img src="/images/anneliesannys.webp" alt="Annelies Annys" />
+        </div>
+        <p class="pb-4">
+          I’m Annelies Annys, a fashion designer and student at KASK & Conservatorium in Ghent. My
+          work focuses on blurring the boundaries between memory, emotion, and materiality. Through
+          collage, text, and clothing, I translate personal experiences into tangible forms.
+        </p>
+        <p class="pb-4">
+          My process often begins with an inner narrative, a dream, a diary fragment, or a fleeting
+          feeling, which gradually develops through experimentation with layers, textures, and form.
+          By using collage as the first step in my design process, I explore composition,
+          proportion, and the relationship between body and form. This method allows me to
+          experiment freely, creating intuitive connections between image, texture, and silhouette
+          before translating them into garments.
         </p>
         <p>
-          Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et
-          commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris.
-          Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula.
-          Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros
-          est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi.
-          Donec fermentum. Pellentesque malesuada nulla a mi.
+          Fashion is a language of reflection and connection: a way to communicate between what is
+          hidden and what is revealed, between my own world and that of others. Each project is an
+          attempt to hold onto what might otherwise be lost, while allowing it to transform into
+          something new.
         </p>
+
+      </div>
+
+      <div class="timeline-section">
+        <!-- TODO: welk titel? -->
+        <!-- <h2 class="timeline-title">Projects</h2> -->
+        <div class="timeline">
+          <!-- 2025 -->
+          <div class="timeline-item">
+            <div class="timeline-year">2025</div>
+            <div class="timeline-events">
+              <div class="timeline-event">
+                Iman Boot, <span class="highlight">Costume Design</span>, Belgium
+              </div>
+              <div class="timeline-event">
+                Stadsduif (Rube Vandebril), <span class="highlight">Costume Design</span>, Belgium
+              </div>
+              <div class="timeline-event">
+                <span class="highlight">POP-UP</span> Shop ChopinChopin, Belgium
+              </div>
+              <div class="timeline-event">
+                KASK <span class="highlight"> fashion show</span>, Belgium
+              </div>
+            </div>
+          </div>
+
+          <div class="timeline-item">
+            <div class="timeline-year">2024</div>
+            <div class="timeline-events">
+              <div class="timeline-event">
+                <span class="highlight">POP-UP</span> Shop Shelter, Belgium
+              </div>
+              <div class="timeline-event">
+                Show window Ida, <span class="highlight">Exhibition</span> , Belgium
+              </div>
+              <div class="timeline-event">
+                Show window Vi-drie-ne, <span class="highlight">Exhibition</span> , Galerie Drie,
+                Belgium
+              </div>
+              <div class="timeline-event">
+                KASK <span class="highlight"> fashion show</span>, Belgium
+              </div>
+            </div>
+          </div>
+
+          <div class="timeline-item">
+            <div class="timeline-year">2023</div>
+            <div class="timeline-events">
+              <div class="timeline-event">
+                KASK <span class="highlight"> fashion show</span>, Belgium
+              </div>
+            </div>
+          </div>
+
+          <div class="timeline-item">
+            <div class="timeline-year">2022</div>
+            <div class="timeline-events">
+              <div class="timeline-event">
+                Making, Healing, Connecting (Soft Connection Lab), Museum Dr. Guislain, Belgium
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <script setup lang="ts"></script>
+
+<style>
+.about-content {
+  display: flex;
+  gap: 60px;
+  padding: 60px 54px;
+  align-items: flex-start;
+  position: relative;
+}
+
+.about-text {
+  color: black;
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 1.5;
+  flex: 1;
+}
+
+.profile-image {
+  float: left;
+  margin-right: 20px;
+}
+
+.profile-image img {
+  width: 300px;
+  min-width: 200px;
+  height: auto;
+  object-fit: cover;
+}
+
+.timeline-section {
+  flex: 1;
+}
+
+.timeline-title {
+  font-size: 24px;
+  font-weight: 400;
+  margin-bottom: 40px;
+  color: black;
+}
+
+.timeline {
+  border-left: 2px solid #d9d9d9;
+  padding-left: 20px;
+  margin-left: 20px;
+}
+
+.timeline-item {
+  margin-bottom: 30px;
+  position: relative;
+}
+
+.timeline-year {
+  font-size: 18px;
+  font-weight: 500;
+  color: black;
+  margin-bottom: 8px;
+}
+
+.timeline-events {
+  margin-left: 0;
+}
+
+.timeline-event {
+  font-size: 16px;
+  font-weight: 300;
+  color: black;
+  margin-bottom: 4px;
+  line-height: 1.4;
+}
+
+/* TODO: welke decoratie? */
+.highlight {
+  color: rgba(255, 182, 193);
+  font-weight: 400;
+}
+</style>
