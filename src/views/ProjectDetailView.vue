21c3998c5fcd444046966916c58e1ff3f8aefa97
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-10-13 19:43:06 +0200

    feat: update project detail view to include studies and enhance project display

--- src/views/ProjectDetailView.vue
+++ src/views/ProjectDetailView.vue
@@ -1,8 +1,16 @@
 <template>
   <div v-if="project">
-    <!-- Show ProjectDetail component for all projects -->
+    <!-- Show StudiesDetail component for projects with studies -->
+    <StudiesDetail
+      v-if="project.studies"
+      :title="project.title"
+      :description="project.description"
+      :studies="project.studies"
+      :video="project.video"
+    />
+    <!-- Show ProjectDetail component for projects with images -->
     <ProjectDetail
-      v-if="project.images"
+      v-else-if="project.images"
       :title="project.title"
       :description="project.description"
       :images="project.images"
@@ -13,6 +21,7 @@
 
 <script setup lang="ts">
 import ProjectDetail from '@/components/ProjectDetail.vue'
+import StudiesDetail from '@/components/StudiesDetail.vue'
 import projectsData from '@/data/projects.json'
 import type { Project } from '@/types/project'
 import { computed, provide } from 'vue'
