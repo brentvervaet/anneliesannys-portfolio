d18cd5bfe5789e483462fe17e3ca5a1c3fd1616a
Brent Vervaet <brent.vervaet@student.hogent.be>
2025-10-19 20:18:38 +0200

    fix: update collage image source to use small-sized version

--- src/components/ProjectDetail.vue
+++ src/components/ProjectDetail.vue
@@ -99,7 +99,7 @@
           <img
             v-for="collage in collages"
             :key="collage.src"
-            :src="getMediumImagePath(collage.src)"
+            :src="getSmallImagePath(collage.src)"
             :alt="collage.alt"
             class="collage-image"
           />
@@ -140,6 +140,12 @@ const getMediumImagePath = (originalPath: string): string => {
   const pathParts = originalPath.split('/')
   const filename = pathParts.pop()
   return [...pathParts, 'med', filename].join('/')
+}
+
+const getSmallImagePath = (originalPath: string): string => {
+  const pathParts = originalPath.split('/')
+  const filename = pathParts.pop()
+  return [...pathParts, 'sm', filename].join('/')
 }
 
 // Sort array alphabetically (case-insensitive)
